e8d163ba19ce0e5d049181ab388dcca3c2a6a159
noless3011 <botay301104@gmail.com>
2026-04-23 21:32:59 +0700

    update board route

--- apps/backend/src/routes/boardRoutes.ts
+++ apps/backend/src/routes/boardRoutes.ts
@@ -1,17 +1,18 @@
 import { Router } from 'express';
+import * as boardController from '../controllers/boardController.js';
 
 const router = Router();
 
 // Domain: Board Management
-router.get('/', (req, res) => { res.send('Not implemented'); });
-router.post('/', (req, res) => { res.send('Not implemented'); });
-router.get('/:boardId', (req, res) => { res.send('Not implemented'); });
-router.patch('/:boardId', (req, res) => { res.send('Not implemented'); });
-router.delete('/:boardId', (req, res) => { res.send('Not implemented'); });
-router.post('/:boardId/thumbnail', (req, res) => { res.send('Not implemented'); });
-router.post('/:boardId/export', (req, res) => { res.send('Not implemented'); });
+router.get('/', boardController.listBoards);
+router.post('/', boardController.createBoard);
+router.get('/:boardId', boardController.getBoard);
+router.patch('/:boardId', boardController.updateBoard);
+router.delete('/:boardId', boardController.deleteBoard);
+router.post('/:boardId/thumbnail', boardController.uploadThumbnail);
+router.post('/:boardId/export', boardController.exportBoard);
 
-// Domain: Collaboration & Sharing
+// Collaboration & Sharing endpoints (to be implemented in collaborationController)
 router.get('/:boardId/collaborators', (req, res) => { res.send('Not implemented'); });
 router.post('/:boardId/collaborators/invite', (req, res) => { res.send('Not implemented'); });
 router.post('/:boardId/collaborators/link', (req, res) => { res.send('Not implemented'); });
@@ -21,16 +22,16 @@ router.delete('/:boardId/collaborators/:userId', (req, res) => { res.send('Not i
 // Standalone invite redeem (can also be in a generic invites router)
 router.post('/api/v1/invites/redeem', (req, res) => { res.send('Not implemented'); });
 
-// Domain: Canvas State REST
+// Canvas State REST (to be implemented in canvasController)
 router.get('/:boardId/canvas/elements', (req, res) => { res.send('Not implemented'); });
 router.put('/:boardId/canvas/snapshot', (req, res) => { res.send('Not implemented'); });
 
-// Domain: Board History
+// Board History (to be implemented in historyController)
 router.get('/:boardId/history', (req, res) => { res.send('Not implemented'); });
 router.get('/:boardId/history/:revisionId', (req, res) => { res.send('Not implemented'); });
 router.post('/:boardId/history/restore', (req, res) => { res.send('Not implemented'); });
 
-// Domain: Active Threads/Chat
+// Active Threads/Chat (to be implemented in threadController)
 router.get('/:boardId/threads', (req, res) => { res.send('Not implemented'); });
 router.post('/:boardId/threads', (req, res) => { res.send('Not implemented'); });
 router.post('/:boardId/threads/:threadId/reply', (req, res) => { res.send('Not implemented'); });
